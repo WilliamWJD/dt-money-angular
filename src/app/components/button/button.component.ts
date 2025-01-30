834fecb7b9e4c7349a1cc929dbc20c933baa14ee
WilliamWJD <william.jose.wjd@gmail.com>
2025-01-29 22:25:49 -0300

    feat: componente header

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component, input, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  title = input<string>('Button');
+}
